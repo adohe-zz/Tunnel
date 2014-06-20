7717c1660929425764281399e24340dd4b848ddc
TonyAdo <coolhzb@163.com>
2014-06-20 11:41:49 +0800

    update the LocalTunnelClient.....

--- src/main/java/com/westudio/java/client/LocalTunnelClient.java
+++ src/main/java/com/westudio/java/client/LocalTunnelClient.java
@@ -1,8 +1,37 @@
 package com.westudio.java.client;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LocalTunnelClient {
 
-    public static void main(String[] argv) {
+    private static final String URL_STRING = "http://localhost/direct-tunnel/";
+    private static final String AUTH = "USERNAME:PASSWORD";
 
+    private static String host;
+    private static String path;
+    private static int port;
+
+    public static void main(String[] args) {
+
+        if (args == null || args.length < 3) {
+            return;
+        }
+
+        URL url;
+        try {
+            url = new URL(URL_STRING);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        host = url.getHost();
+        port = url.getPort() < 0 ? url.getDefaultPort() : url.getPort();
+        path = url.getPath();
+
+        while (true) {
+
+        }
     }
 }
