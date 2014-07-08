4a3e21c63dc50c89861da44551653cad94c63964
TonyAdo <coolhzb@163.com>
2014-07-08 18:44:21 +0800

    DirectTunnelServlet add onRecvClient function ....

--- src/main/java/com/westudio/java/server/DirectTunnelServlet.java
+++ src/main/java/com/westudio/java/server/DirectTunnelServlet.java
@@ -14,6 +14,12 @@ public class DirectTunnelServlet extends TunnelServlet {
     private static final int DEFAULT_SEGMENT_SIZE = 32758;
     private static final byte[] EMPTY_CHUNK = {0, 0};
 
+    private static void closeSocket(final Socket socket) {
+        try {
+            socket.close();
+        } catch (IOException e) {/**/}
+    }
+
     private static void onRecvRemote(InputStream is, OutputStream os) throws IOException {
         byte[] buffer  = new byte[DEFAULT_SEGMENT_SIZE + 2];
         long lastSent = 0;
@@ -46,8 +52,33 @@ public class DirectTunnelServlet extends TunnelServlet {
         }
     }
 
-    private static void onRecvClient(InputStream is, OutputStream os) {
+    private static void onRecvClient(InputStream is, OutputStream os) throws IOException {
         byte[] buffer = new byte[DEFAULT_SEGMENT_SIZE];
+        int b0;
+        int b1;
+
+        while ((b0 = is.read()) > 0 && (b1 = is.read()) > 0) {
+            int len = (b0 << 8) + b1;
+            if (len == 0) {
+                // Heartbeat
+                continue;
+            }
+
+            if (len > DEFAULT_SEGMENT_SIZE) {
+                break;
+            }
+
+            int off = 0;
+            while (len > 0) {
+                int bytesRead = is.read(buffer, off, len);
+                if (bytesRead < 0) {
+                    break;
+                }
+                off += bytesRead;
+                len -= bytesRead;
+            }
+            os.write(buffer, 0, off);
+        }
     }
 
     @Override
@@ -84,10 +115,16 @@ public class DirectTunnelServlet extends TunnelServlet {
                 try {
                     onRecvRemote(socket_.getInputStream(), res.getOutputStream());
                 } catch (IOException e) {/**/}
+                closeSocket(socket_);
             }
         });
 
-
+        try {
+            onRecvClient(req.getInputStream(), socket.getOutputStream());
+        } catch (IOException e) {
+        } finally {
+        }
+        closeSocket(socket);
     }
 
 }
