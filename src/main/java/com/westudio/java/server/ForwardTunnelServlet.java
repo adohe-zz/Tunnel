2f700e9ef4aec7efa1fa2aecb944dcc1afdd4845
Tony <coolhzb@163.com>
2014-07-11 22:01:47 +0800

    update the ForwardTunnelServlet….

--- src/main/java/com/westudio/java/server/ForwardTunnelServlet.java
+++ src/main/java/com/westudio/java/server/ForwardTunnelServlet.java
@@ -1,12 +1,22 @@
 package com.westudio.java.server;
 
+import com.westudio.java.util.Numbers;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ForwardTunnelServlet extends TunnelServlet {
 
     @Override
     protected void doTunnel(HttpServletRequest req, HttpServletResponse res) {
+        final int port = Numbers.parseInt(req.getHeader("Listen"));
 
+        if (port < 0) {
+            try {
+                res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            } catch (IOException e) {/**/}
+            return;
+        }
     }
 }
