2a64b85b8a8127882ae1cc8fefab6155f73fe184
Tony <coolhzb@163.com>
2014-07-11 21:55:11 +0800

    Executors rewrite the execute function….

--- src/main/java/com/westudio/java/util/Executors.java
+++ src/main/java/com/westudio/java/util/Executors.java
@@ -3,12 +3,19 @@ package com.westudio.java.util;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Executors {
 
     private static final int POOL_SIZE = 50;
 
+    private static AtomicInteger threadNum = new AtomicInteger(0);
+
     private static LinkedBlockingDeque<Runnable> queue = new LinkedBlockingDeque<>();
+
+    public static int getThreadNum() {
+        return threadNum.get();
+    }
 
     private static Lazy<ThreadPoolExecutor> executor = new Lazy<ThreadPoolExecutor>() {
         @Override
@@ -28,8 +35,20 @@ public class Executors {
         }
     };
 
-    public static void execute(Runnable runnable) {
-        executor.getInstance().execute(runnable);
+    public static void execute(final Runnable runnable) {
+        executor.getInstance().execute(new Runnable() {
+            @Override
+            public void run() {
+                threadNum.incrementAndGet();
+                try {
+                    runnable.run();
+                } catch (RuntimeException e) {
+
+                } finally {
+                    threadNum.decrementAndGet();
+                }
+            }
+        });
     }
 
     public static void shutdown() {
